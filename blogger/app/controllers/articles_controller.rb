890cbbcbe7249d2ad5f190db05a311abc9292008
Kyankya Raymond <rkyankya@rkyankya.rkyankya>
2020-01-10 20:48:07 +0300

    Added Delete Option

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -6,14 +6,20 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
     def new
         @article = Article.new
     end
+
     def create
         @article = Article.new(article_params)
             #title: params[:article][:title]
             #body: params[:article][:title])
         @article.save 
         redirect_to article_path(@article)
+    end
+
+    def destroy
+        @article = Article.destroy(params[:id])
     end
 end
