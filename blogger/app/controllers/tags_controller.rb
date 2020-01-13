1505d8bab16aaed7818a6c2ad9172392e9d05be8
Kyankya Raymond <rkyankya@rkyankya.rkyankya>
2020-01-13 16:40:59 +0300

    Partial Fix for Tag

--- blogger/app/controllers/tags_controller.rb
+++ blogger/app/controllers/tags_controller.rb
@@ -1,5 +1,13 @@
 class TagsController < ApplicationController
+  def index
+    @tags = Tag.all
+  end
+
   def show
     @tag = Tag.find(params[:id])
+  end
+
+  def new
+    @tag = Tag.new
   end
 end
